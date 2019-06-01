03e8e5351b245e22d102c72f23c86fd67f49aac9
FAmura <federicoamura@gmail.com>
2019-06-01 16:17:52 -0300

    PONE LOS ";" AGUSTIN LPM!!!
    Cambia el grupo de choferes a 3
    Pone el estado del viaje en curso a "buscando chofer" despues de que este se rechaza por el chofer confirmado (ya que todavia no lo confirmo)
    Le pone tambien el nuevo chofer asignado al viaje

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -79,8 +79,7 @@ async function getAvailableDrivers(): Promise<any> {
 export const user = functions.https.onRequest(userExpress);
 export const findUser = functions.https.onCall(async (data, context) => {
   const fbid = data.fbid;
-  const response = await getUser(fbid);
-  return response;
+  return await getUser(fbid);
 });
 export const asignarChoferes = functions.database.ref('/viajes/{id}').onCreate(async (snapshot, context) => {
   let lat = snapshot.val().origin_latitude;
@@ -95,8 +94,8 @@ export const asignarChoferes = functions.database.ref('/viajes/{id}').onCreate(a
       let d = ((lat - lat2) ^ 2 + (lon - lon2) ^ 2) ^ 0.5;
       return {key: k, distance: d};
     });
-    distances.sort((x, y) => (x.distance > y.distance) ? 1 : -1)
-    let final: any = {}
+    distances.sort((x, y) => (x.distance > y.distance) ? 1 : -1);
+    let final: any = {};
     for (let i = 0; i < distances.length; i++) {
       final[i] = distances[i].key;
     }
@@ -107,63 +106,65 @@ export const asignarChoferes = functions.database.ref('/viajes/{id}').onCreate(a
 });
 export const rechazarViaje = functions.https.onCall(async (data, context) => {
   const viajeid = data.viajeid;
-  const intentosDS = await database.ref("viajes").child(viajeid).child("intentosAsignacion").once("value")
-  const intentos: number = intentosDS.val()
-  const cantChofDS = await database.ref("viajes").child(viajeid).child("cantChoferes").once("value")
-  const cantChoferes = cantChofDS.val()
-  let pos = 0
-  if (intentos % cantChoferes == 0) pos = cantChoferes - 1
-  else pos = (intentos % cantChoferes) - 1
-  const choferidDS = await database.ref("viajes").child(viajeid).child("drivers").child(pos.toString()).once("value")
-  const choferid = choferidDS.val()
+  const intentosDS = await database.ref("viajes").child(viajeid).child("intentosAsignacion").once("value");
+  const intentos: number = intentosDS.val();
+  const cantChofDS = await database.ref("viajes").child(viajeid).child("cantChoferes").once("value");
+  const cantChoferes = cantChofDS.val();
+  let pos = 0;
+  if (intentos % cantChoferes == 0) pos = cantChoferes - 1;
+  else pos = (intentos % cantChoferes) - 1;
+  const choferidDS = await database.ref("viajes").child(viajeid).child("drivers").child(pos.toString()).once("value");
+  const choferid = choferidDS.val();
 
-  console.log(choferid)
-  await database.ref("drivers").child(choferid).child("viajeAsignado").remove()
-  console.log("asd")
+  console.log(choferid);
+  await database.ref("drivers").child(choferid).child("viajeAsignado").remove();
 
   if (intentos < cantChoferes * 2) {
-    if (intentos % 1 == 0)
-      await database.ref("viajes").child(viajeid).child("estado").set(90)
+    if (intentos % 3 == 0)
+      await database.ref("viajes").child(viajeid).child("estado").set(90);
     else {
       const nextchoferidDS = await database.ref("viajes").child(viajeid).child("drivers").child((intentos % cantChoferes).toString()).once("value")
-      const nextchoferid = nextchoferidDS.val()
-      await database.ref("drivers").child(nextchoferid).child("viajeAsignado").set(viajeid)
-      await database.ref("viajes").child(viajeid).child("intentosAsignacion").set(intentos + 1)
+      const nextchoferid = nextchoferidDS.val();
+      await database.ref("viajes").child(viajeid).child("estado").set(0);
+      await database.ref("viajes").child(viajeid).child("intentosAsignacion").set(intentos + 1);
+      await database.ref("viajes").child(viajeid).child("chofer").set(nextchoferid);
+      await database.ref("drivers").child(nextchoferid).child("viajeAsignado").set(viajeid);
     }
   } else {
-    await database.ref("viajes").child(viajeid).child("estado").set(999)
+    await database.ref("viajes").child(viajeid).child("estado").set(999);
   }
 });
 export const cancelarViaje = functions.https.onCall(async (data, context) => {
-  const viajeid = data.viajeid
-  const intentosDS = await database.ref("viajes").child(viajeid).child("intentosAsignacion").once("value")
-  const intentos: number = intentosDS.val()
-  const cantChofDS = await database.ref("viajes").child(viajeid).child("cantChoferes").once("value")
-  const cantChoferes = cantChofDS.val()
-  let pos = 0
-  if (intentos % cantChoferes == 0) pos = cantChoferes - 1
-  else pos = (intentos % cantChoferes) - 1
-  const choferidDS = await database.ref("viajes").child(viajeid).child("drivers").child(pos.toString()).once("value")
-  const choferid = choferidDS.val()
+  const viajeid = data.viajeid;
+  const intentosDS = await database.ref("viajes").child(viajeid).child("intentosAsignacion").once("value");
+  const intentos: number = intentosDS.val();
+  const cantChofDS = await database.ref("viajes").child(viajeid).child("cantChoferes").once("value");
+  const cantChoferes = cantChofDS.val();
+  let pos = 0;
+  if (intentos % cantChoferes == 0) pos = cantChoferes - 1;
+  else pos = (intentos % cantChoferes) - 1;
+  const choferidDS = await database.ref("viajes").child(viajeid).child("drivers").child(pos.toString()).once("value");
+  const choferid = choferidDS.val();
 
-  await database.ref("drivers").child(choferid).child("viajeAsignado").remove()
+  await database.ref("drivers").child(choferid).child("viajeAsignado").remove();
 
-  await database.ref("viajes").child(viajeid).child("estado").set(20)
-})
+  await database.ref("viajes").child(viajeid).child("estado").set(20);
+});
+
 export const seguirBuscando = functions.https.onCall(async (data, context) => {
   const viajeid = data.viajeid;
-  const intentosDS = await database.ref("viajes").child(viajeid).child("intentosAsignacion").once("value")
-  const intentos: number = intentosDS.val()
-  const cantChofDS = await database.ref("viajes").child(viajeid).child("cantChoferes").once("value")
-  const cantChoferes = cantChofDS.val()
-  let pos = intentos % cantChoferes
-  const choferidDS = await database.ref("viajes").child(viajeid).child("drivers").child(pos.toString()).once("value")
-  const choferid = choferidDS.val()
+  const intentosDS = await database.ref("viajes").child(viajeid).child("intentosAsignacion").once("value");
+  const intentos: number = intentosDS.val();
+  const cantChofDS = await database.ref("viajes").child(viajeid).child("cantChoferes").once("value");
+  const cantChoferes = cantChofDS.val();
+  let pos = intentos % cantChoferes;
+  const choferidDS = await database.ref("viajes").child(viajeid).child("drivers").child(pos.toString()).once("value");
+  const choferid = choferidDS.val();
 
-  await database.ref("viajes").child(viajeid).child("estado").set(0)
-  await database.ref("viajes").child(viajeid).child("intentosAsignacion").set(intentos + 1)
+  await database.ref("viajes").child(viajeid).child("estado").set(0);
+  await database.ref("viajes").child(viajeid).child("intentosAsignacion").set(intentos + 1);
   await database.ref("drivers").child(choferid).child("viajeAsignado").set(viajeid);
-})
+});
 
 
 // cuando usuario cancela, limpiar pantalla a chofer
